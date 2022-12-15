b0c88281fa9c097a369458d74c309bad6091fca1
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-15 11:40:36 -0300

    Definida estrutura da Home Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Product from "../components/Product.svelte";
+</script>
+<svelte:head>
+    <title>vtFood - Home</title>
+</svelte:head>
+
+	<Product />
+
+<style>
+	
+</style>
