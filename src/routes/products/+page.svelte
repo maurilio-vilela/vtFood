48256c09a9ee8a1be61d1eff8af28106457e7bf4
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-15 15:45:29 -0300

    Criado a rota para products

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Col, Row } from "sveltestrap";
+    import Product from "../../components/Product.svelte";
+</script>
+<svelte:head>
+    <title>vtFood - Produtos</title>
+</svelte:head>
+
+<Row>
+    <Col xs="12">
+    <h1 class="title">-Hamburguers</h1>
+	<Product />
+    </Col>
+</Row>
+<style>
+    :global()
+	.title{
+        display: flex;
+        text-transform: uppercase;
+        font-family:'Poppins';
+        font-weight: 400;
+        font-size: 1.8rem;
+        color: #FFFFFF;
+        justify-content: flex-start;
+        margin-bottom: 1.3rem;
+    }
+</style>
