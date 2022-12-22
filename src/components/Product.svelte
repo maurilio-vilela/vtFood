2a8bde663469632ceac3c0bc16c8088d0556d3b1
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-22 11:59:30 -0300

    Create the function getProducts

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,5 +1,32 @@
-<script>
+<script lang="ts">
+	import { supabase } from "$lib/db";
+    import { onMount } from "svelte";
     import { Badge, Col, Figure, Icon, Image, Row } from "sveltestrap";
+
+	let loading = false
+	let name = ""
+	let description = ""
+	let image = ""
+	let price = ""
+
+	onMount(() => {
+    	getProducts()
+	})
+
+	const getProducts = async () => {
+		loading = true
+		const { data:products, error } = await supabase
+		.from('products')
+		.select('*')
+		if (products){
+			console.log("Deu certo")
+			name = products.name
+			description = products.description
+			image = products.image
+			price = products.price
+		}else{
+            console.log(error)
+        }
 </script>
 <Row cols={{ lg: 3, md: 2, sm: 1 }} class="g-3">
 	<!-- Product Item Start-->
@@ -12,19 +39,19 @@
 		</Row>
 		<Row>
 			<Col class="title">
-				Hamburger Artesanal
+				{name}
 			</Col>
 		</Row>
 		<Row>
 			<Col>
-				<Figure caption="Pão de brioche, carne de 150g, 2 fatias e bacon, salada e queijo cheddar.">
-					<Image fluid alt="Hamburger Artesanal" src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg" />
+				<Figure caption={description}>
+					<Image fluid alt="Hamburger Artesanal" src={image}/>
 				</Figure>
 			</Col>
 		</Row>
 		<Row class="bottom-product">
 			<Col xs="9">
-				R$ 250,00
+				{price}
 			</Col>
 			<Col xs="3" class="add">
 				<a href="#" class="btn btn-danger">
