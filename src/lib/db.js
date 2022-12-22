93c1ca7d9d23f9ca0d61c265f23d18de2b9b7458
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-22 11:58:29 -0300

    Alterado para acesso direto aos dados do supabase

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(
+    "https://ykhqaotqugdtidnepnfq.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlraHFhb3RxdWdkdGlkbmVwbmZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwNDM3MDcsImV4cCI6MTk4NjYxOTcwN30.lJcEJ89HFoJ9U5vwDX1VXh_O2zyhBmxHWSyLGSDaPDA"
+    )
